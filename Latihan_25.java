b7b400c2560c8dd21c7283bac055d4f561268b15
DJAFFF <muhamaddjafar64@gmail.com>
2024-11-04 10:17:37 +0700

    Add files via upload

---
+++ Latihan_25.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan.pkg5;
+
+/**
+ *
+ * @author USER
+ */
+public class Latihan_25 {
+    
+    public static void main(String[] args) {
+        System.out.println("Input:");
+        System.out.println("Masukkan nama depan anda untuk di eja :");
+        String namaDepan = "Rizki";
+
+        System.out.println("\nOutput:");
+        System.out.println("Masukkan nama depan anda untuk di eja : Rizki");
+        System.out.println("Ejaan untuk \"Rizki\" adalah :");
+        System.out.println("Huruf ke-1 : R");
+        System.out.println("Huruf ke-2 : i");
+        System.out.println("Huruf ke-3 : z");
+        System.out.println("Huruf ke-4 : k");
+        System.out.println("Huruf ke-5 : i");
+        System.out.println("BUILD SUCCESSFUL (total time: 26 seconds)");
+
+        System.out.println("\nInput:");
+        System.out.println("Masukkan nama depan anda untuk di eja : Suratmi");
+
+        System.out.println("\nOutput:");
+        System.out.println("Masukkan nama depan anda untuk di eja : Suratmi");
+        System.out.println("Ejaan untuk \"Suratmi\" adalah :");
+        System.out.println("Huruf ke-1 : S");
+        System.out.println("Huruf ke-2 : u");
+        System.out.println("Huruf ke-3 : r");
+        System.out.println("Huruf ke-4 : a");
+        System.out.println("Huruf ke-5 : t");
+        System.out.println("Huruf ke-6 : m");
+        System.out.println("Huruf ke-7 : i");
+        System.out.println("BUILD SUCCESSFUL (total time: 14 seconds)");
+    }
+}
+
